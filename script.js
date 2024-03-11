f7ba1903ce8e2e45b10a9675d56a54fbe1db2021
Prerak Madan <madanjatin2004@gmail.com>
2024-03-11 19:01:37 +0530

    add changes

--- script.js
+++ script.js
@@ -12,3 +12,44 @@
   };
 
   document.getElementById('generate').addEventListener('click', generateLoremIpsum);
+  import React, { useState } from 'react';
+
+const LoremGenerator = () => {
+  const [numParagraphs, setNumParagraphs] = useState(1);
+  const [generatedText, setGeneratedText] = useState('');
+
+  const generateLoremIpsum = () => {
+    fetch(`https://baconipsum.com/api/?type=meat-and-filler&paras=${numParagraphs}`)
+      .then(response => response.json())
+      .then(data => setGeneratedText(data.join('\n\n')))
+      .catch(error => console.error('Error fetching Lorem Ipsum:', error));
+  };
+
+  return (
+    <div>
+      <h1>Lorem Generator</h1>
+      <label htmlFor="numParagraphs">Number of Paragraphs:</label>
+      <input
+        type="number"
+        id="numParagraphs"
+        value={numParagraphs}
+        onChange={e => setNumParagraphs(parseInt(e.target.value))}
+        min={1}
+        max={10}
+      />
+      <button onClick={generateLoremIpsum}>Generate Lorem Ipsum</button>
+      <div>
+        <textarea
+          rows={10}
+          cols={50}
+          value={generatedText}
+          readOnly
+          style={{ marginTop: '10px' }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoremGenerator;
+
